a8744495c19c431a2813eadace6e62abaccdd10c
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-10-18 16:33:35 +0530

    added crud operations : update user-age by name, delete user by name

--- routes/routes.js
+++ routes/routes.js
@@ -10,5 +10,9 @@ router.get("/get-users",controller.getUsers)
 router.get("/get-by-name/:fname",controller.getUserByName)
 router.get("/get-name-and-age",controller.getNameAndAge)
 
+router.put("/update-age-by-name/:fname", controller.updateAgeByname)
+router.put("/update-inc-age-by-name", controller.updateAgeByName2)
+
+router.delete("/delete-by-name/:fname", controller.deleteByName)
 
 module.exports = router
